d1050e0691d58628c774d429a4db60729d147b99
Dhruba <sikdardhrubanil@gmai.com>
2021-02-04 18:00:39 +0530

    adding message

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ const router = require("./Routers/router")
 
 const app = express();
 const port = process.env.PORT || 5000;
+// console.log(path.join(__dirname, "../clientside/build"));
 
 
 app.use(express.json());
 app.use(cors())
 app.use(router)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../ clientside / build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../clientside/build/index.html"))
+    })
+}
+
 app.listen(port, "127.0.0.1", () => {
     console.log(`server running at port ${port}`);
 })
